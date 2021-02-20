712007a0a55aea1eeb50e5f2880b956330d6a57c
Simon Schueller <sps67@pitt.edu>
2021-02-20 12:22:51 -0500

    create new class form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function editCourses() {
-  var buttons = document.getElementsByClassName("remove-btn");
-  var editBtn = document.getElementById("edit-btn");
+var buttons = document.getElementsByClassName("remove-btn");
+var editBtn = document.getElementById("edit-btn");
+
+editBtn.onclick = function() {
   if (buttons[0].style.display === "none") {
     for(var i = 0; i<buttons.length; i++){
       buttons[i].style.display = "block";
